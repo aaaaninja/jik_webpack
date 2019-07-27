8778181c6f0c2089c393a13b9effb4f732eac00e
aaaaninja <keshigomu71505@gmail.com>
2019-07-27 20:00:46 +0900

    :wrench: 生成ファイルの出力先をpublic/jsからdistに変更

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 , entry: "./src/js/app.js"
 , output: {
     filename: "bundle.js"
-  , path: path.join(__dirname, "public/js")
+  , path: path.join(__dirname, "dist/")
   }
 , module: {
     rules: [
